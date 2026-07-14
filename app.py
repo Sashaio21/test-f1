5f8a039a0802b4b2e6acb33ced323fd953f5f52c
Alexandr <alexandrkowal21@gmail.com>
2026-07-14 22:04:38 +0300

    fsdf

--- app.py
+++ app.py
@@ -1,51 +1,44 @@
-import fastf1
-import matplotlib.pyplot as plt
+import requests
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
-
-fastf1.Cache.enable_cache("cache")
 
 
 @app.route("/")
 def index():
 
-    session = fastf1.get_session(2024, "Monza", "Q")
-    session.load()
-
-    lec_lap = session.laps.pick_driver("LEC").pick_fastest()
-    ver_lap = session.laps.pick_driver("VER").pick_fastest()
-
-    lec_tel = lec_lap.get_car_data().add_distance()
-    ver_tel = ver_lap.get_car_data().add_distance()
-
-    plt.figure(figsize=(12, 6))
-
-    plt.plot(
-        lec_tel["Distance"],
-        lec_tel["Speed"],
-        label="Leclerc"
+    url = (
+        "https://livetiming.formula1.com/static/"
+        "2024/2024-07-28_Belgian_Grand_Prix/"
+        "2024-07-27_Qualifying/"
+        "SessionInfo.jsonStream"
     )
 
-    plt.plot(
-        ver_tel["Distance"],
-        ver_tel["Speed"],
-        label="Verstappen"
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    try:
+        response = requests.get(
+            url,
+            # headers=headers,
+            timeout=20
+        )
+
+        status = response.status_code
+
+        data = response.text[:1000]
+
+    except Exception as e:
+        status = "ERROR"
+        data = str(e)
+
+
+    return render_template(
+        "index.html",
+        status=status,
+        data=data
     )
-
-    plt.xlabel("Distance (m)")
-    plt.ylabel("Speed (km/h)")
-    plt.title("Monza 2024 Qualifying")
-    plt.legend()
-
-    os.makedirs("static", exist_ok=True)
-
-    plt.savefig("static/speed.png")
-
-    plt.close()
-
-    return render_template("index.html")
 
 
 if __name__ == "__main__":
